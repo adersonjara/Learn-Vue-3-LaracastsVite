8c6b86c7be89145fb56b17b58c6d6621e2f940dd
adersonjara <adersonjara15@gmail.com>
2023-01-30 18:09:21 -0500

    Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
 <script setup>
 import TheWelcome from "@/components/TheWelcome.vue";
+
+
+let message = $ref("Hello, World");
+
+setTimeout(() => {
+	message = "I have Been Changed";
+},2000);
+
+let doSomething = () => {
+	alert("doing it now");
+};
+
+
+//Sin colocar setup en <script setup>
+/*export default {
+	setup(){
+		//onMounted(() => {
+		//	alert("Hi there");
+		//});
+
+		let message = ref("Hello, World");
+
+		setTimeout(() => {
+			message.value = "I have Been Changed";
+		},2000);
+
+		return {
+			message
+		};
+	}
+};*/
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    {{ message }}
+    <input type="text" v-model="message">
+    <button @click="doSomething">Click me</button>
   </main>
 </template>
